6692af084a49794250048f097926fd3140292893
samumar43 <samumar43@gmail.com>
2020-06-29 18:19:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(kitten) {
+  const appendKitten = kittens.concat(kitten);
+  return appendKitten;
+}
